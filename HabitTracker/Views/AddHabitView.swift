e5a3cce54081833c6222604c5af0ddfe66940a1f
AtinAgnihotri <atinagnihotri@gmail.com>
2021-06-19 18:39:54 +0530

    Adds AddHabitView

---
+++ HabitTracker/Views/AddHabitView.swift
@@ -0,0 +1,20 @@
+//
+//  AddHabitView.swift
+//  HabitTracker
+//
+//  Created by Atin Agnihotri on 19/06/21.
+//
+
+import SwiftUI
+
+struct AddHabitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddHabitView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddHabitView()
+    }
+}
---
+++ HabitTracker/Views/HabitDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  HabitDetailsView.swift
+//  HabitTracker
+//
+//  Created by Atin Agnihotri on 19/06/21.
+//
+
+import SwiftUI
+
+struct HabitDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HabitDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        HabitDetailsView()
+    }
+}
