497793289832bf25ff5aeddb3a68431a5fff5872
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-19 18:16:30 +0530

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Atin Agnihotri on 19/06/21.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
